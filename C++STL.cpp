81b7537c2e3a7dd6336ef7542d92a9e0ef7ffbf5
Nishad <iamnishadsharma13@gmail.com>
2023-10-07 18:51:41 +0530

    Add files via upload
    C++ Standard Libraries

---
+++ C++STL.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>  //Universal header file. Used to program with all statndard libraries.
+using namespace std;
+
+//*CONTAINERS*
+//PAIRS
+
+//void explainPairs()
+//{
+//	pair<int ,int>p = {1,3};
+//	cout << p.first << " " << p.second;
+//	
+//	pair<int , pair<int , int>>p = {1,{3,4}}
+//	cout << p.first << " " << p.second.first << " " << p.second.second;  //Nested Pair
+//	
+//	pair<int , int>arr[] = {{1,2},{3,4},{5,6}};
+//	cout << arr[1].second;
+//	
+//}
+
+
+//VECTORS
+
+//int main()
+//{
+//	vector<int>v;
+//	for(int i=0 ; i<=10 ; i++)
+//	{
+//		v.push_back(i);
+//	}
+//	
+//	cout << "The elements in the vector: ";
+//	for (auto it = v.begin(); it != v.end(); it++)
+//    {
+//        cout << * it << " ";
+//    }
+//    
+//    cout << "\nThe front element of the vector: " << v.front() << endl;
+//    cout << "The last element of the vector: " << v.back() << endl;
+//    cout << "The size of the vector: "<< v.size() << endl;
+//    cout << "Deleting element from the end: " << v[v.size() - 1];
+//    v.pop_back();
+//    
+//    cout << "\nPrinting the vector after the removal of the last element: ";
+//    for(int i = 0 ; i < v.size() ; i++)
+//    {
+//        cout << v[i] << " ";
+//    }
+//    
+//    cout << "\nInsert 5 at the beginning ";
+//    v.insert(v.begin(),5);
+//    cout << "\nThe first element is: "<< v[0] << endl;
+//    cout << "Erasing the first element" << endl;
+//    v.erase(v.begin());
+//    cout << "Current first element: " << v[0] << endl;
+//    
+//    if(v.empty())
+//    {
+//        cout << "The vector is empty.";
+//    }
+//    else
+//    {
+//        cout << "The vector is not empty. ";
+//    }
+//    
+//    v.clear();
+//    cout << "\nSize after deleting whole elements of the vector: " << v.size();
+//}
